3c7346123c3d71a3fa0724dbea009a245ef93466
zhangyuanyuan <zhangyuanyuan@hoxigames.com>
2025-11-10 17:57:44 +0800

    exc last call and set singleton

--- lib/screenshot_callback.dart
+++ lib/screenshot_callback.dart
@@ -3,15 +3,17 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class ScreenshotCallback {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter.moum/screenshot_callback');
+  static final ScreenshotCallback _singleton = ScreenshotCallback._internal();
+  static ScreenshotCallback get instance => _singleton;
+
+  ScreenshotCallback._internal() {
+    initialize();
+  }
+
+  static const MethodChannel _channel = const MethodChannel('flutter.moum/screenshot_callback');
 
   /// Functions to execute when callback fired.
   List<VoidCallback> onCallbacks = <VoidCallback>[];
-
-  ScreenshotCallback() {
-    initialize();
-  }
 
   /// Initializes screenshot callback plugin.
   Future<void> initialize() async {
@@ -24,12 +26,20 @@ class ScreenshotCallback {
     onCallbacks.add(callback);
   }
 
+  /// Remove void callback.
+  void removeListener(VoidCallback callback) {
+    onCallbacks.remove(callback);
+  }
+
   Future<dynamic> _handleMethod(MethodCall call) async {
     switch (call.method) {
       case 'onCallback':
-        for (final callback in onCallbacks) {
-          callback();
-        }
+        // for (final callback in onCallbacks) {
+        //   callback();
+        // }
+        // exc last call
+        final callback = onCallbacks.lastOrNull;
+        callback?.call();
         break;
       default:
         throw ('method not defined');
